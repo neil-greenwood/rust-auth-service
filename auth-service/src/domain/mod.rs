a5fc90c47a025b1a0ed6b8b3b77b12e2bf27f25a
Neil Greenwood <gneel42@gmail.com>
2026-02-15 12:32:52 +0000

    [Sprint 2][Task 4] feat - implement Password type-driven design.

--- auth-service/src/domain/mod.rs
+++ auth-service/src/domain/mod.rs
@@ -1,10 +1,12 @@
 mod data_stores;
 mod email;
 mod error;
+mod password;
 mod user;
 
 // re-export items from sub-modules
 pub use data_stores::*;
 pub use email::*;
 pub use error::*;
+pub use password::*;
 pub use user::*;
