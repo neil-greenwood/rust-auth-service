f77f6d1cd81fb30edce31901294a0abd71af897a
Neil Greenwood <gneel42@gmail.com>
2026-04-03 15:31:50 +0100

    [Sprint 5][Task 1] feat: implement postgresql db.

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,12 +1,14 @@
 use auth_service::{
     app_state::AppState,
+    get_postgres_pool,
     services::{
         hashmap_2fa_code_store::HashmapTwoFACodeStore, hashmap_user_store::HashmapUserStore,
         hashset_banned_token_store::HashsetBannedTokenStore, mock_email_client::MockEmailClient,
     },
-    utils::constants::prod,
+    utils::constants::{prod, DATABASE_URL},
     Application,
 };
+use sqlx::PgPool;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
@@ -16,6 +18,7 @@ async fn main() {
     let banned_token_store = HashsetBannedTokenStore::default();
     let two_fa_codes = HashmapTwoFACodeStore::default();
     let email_client = MockEmailClient {};
+    let pg_pool = configure_postgresql().await;
     let app_state = AppState::new(
         Arc::new(RwLock::new(user_store)),
         Arc::new(RwLock::new(banned_token_store)),
@@ -30,4 +33,16 @@ async fn main() {
         .await
         .expect("Failed to build app");
     app.run().await.expect("Failed to run app");
+}
+
+async fn configure_postgresql() -> PgPool {
+    let pg_pool = get_postgres_pool(&DATABASE_URL)
+        .await
+        .expect("Failed to create Postgres connection pool!");
+    sqlx::migrate!()
+        .run(&pg_pool)
+        .await
+        .expect("Failed to run migrations");
+
+    pg_pool
 }
